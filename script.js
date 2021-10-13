c733080f05c3ee0e12f8053031ad982fb333ff43
enamul1996 <enamulhussain36@gmail.com>
2021-10-13 09:54:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let scorep1 = document.querySelector('#score--0')
+let scorep2 = document.querySelector('#score--1')
+let currentScorep1 = document.querySelector('#current--1');
+let currentScorep2 = document.querySelector('#current--0');
+
+let diceOne = document.querySelector('.dice')
+let btnNew = document.querySelector('.btn--new')
+let holdButton = document.querySelector('.btn--hold')
+let rollDice = document.querySelector('.btn--roll')
+
+let playing, scores, currentScore, activePlayer
+
+let init = function(){
+    scores=[0,0]
+    currentScore = 0
+    activePlayer = 0
+    playing = true
+
+    scorep1.textContent = 0
+    scorep2.textContent = 0
+    currentScorep1.textContent = 0
+    currentScorep2.textContent = 0
+
+    document.querySelector(`#name--0`).textContent = "Player-1"
+    document.querySelector(`#name--1`).textContent = "Player-2"
+}
+
+init()
+btnNew.addEventListener('click',init)
+rollDice.addEventListener('click',function(){
+
+    if(playing){
+        let dice = Math.floor(Math.random()*6)+1
+        diceOne.src = `dice-${dice}.png`;
+
+        if(dice !== 1){
+            currentScore = currentScore + dice 
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore
+            
+        }
+        else{
+              switchPlayer()
+        }
+    }
+})
+
+function switchPlayer(){
+     document.querySelector(`#current--${activePlayer}`).textContent = 0
+     currentScore = 0
+     activePlayer =  activePlayer === 0 ? activePlayer = 1:activePlayer = 0
+ }   
+
+ holdButton.addEventListener('click',function(){
+
+     scores[activePlayer] = scores[activePlayer] + currentScore
+
+     document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer]
+     
+    if(scores[activePlayer] >= 20){
+        document.querySelector(`#name--${activePlayer}`).textContent = "Winner"
+        playing = false
+    }
+    
+    switchPlayer()
+})
+
+
+function switchPlayer(){
+   document.querySelector(`#current--${activePlayer}`).textContent = 0
+   currentScore = 0
+   activePlayer = activePlayer === 0 ? activePlayer = 1:activePlayer = 0
+
+}
+
+
